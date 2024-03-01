138ae5de5ba93bd744a18e661b1dc366addd0326
shireinnn <aaliyah.shirein@gmail.com>
2024-03-01 08:07:31 +0700

    praktik01

---
+++ Praktik_01/MainMobil.java
@@ -0,0 +1,13 @@
+public class MainMobil{
+public static void main(String[] args) {
+    
+ Mobil mobil1 = new Mobil();
+ 
+     mobil1.merk = "Innova";
+     mobil1.platnomor = "N 1987 GH";
+     mobil1.tahun = 2016;
+     mobil1.transmisi = "Automatic";
+     mobil1.maju();
+
+    }
+}
---
+++ Praktik_01/Mobil.java
@@ -0,0 +1,9 @@
+public class Mobil {
+    public String merk;
+    public String platnomor;
+    public int tahun;
+    public String transmisi;
+    public void maju(){
+        System.out.printf("mobil %s dengan plat nomor %s bergerak maju", merk, platnomor);
+    }
+}
